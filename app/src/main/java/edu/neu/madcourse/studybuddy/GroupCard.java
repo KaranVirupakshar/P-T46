ba9a0ebd1011afd498122556082603f74843483d
nikhilfeb95 <nikhil.mollay@gmail.com>
2022-04-16 16:36:08 -0400

    First commit for the home screen and the cards

---
+++ app/src/main/java/edu/neu/madcourse/studybuddy/GroupCard.java
@@ -0,0 +1,21 @@
+package edu.neu.madcourse.studybuddy;
+
+import android.view.View;
+import android.widget.AdapterView;
+
+/**
+ * A class that represents the group a student has
+ */
+public class GroupCard implements AdapterView.OnItemClickListener {
+
+    String title;
+    String date;
+    String location;
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+    }
+
+
+}
