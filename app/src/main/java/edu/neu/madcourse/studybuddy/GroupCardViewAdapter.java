01c0e27078ca9e44a9afae35cc92ef482813c41d
nikhilfeb95 <nikhil.mollay@gmail.com>
2022-04-17 01:54:38 -0400

    Adding some changes to the home screen

--- app/src/main/java/edu/neu/madcourse/studybuddy/GroupCardViewAdapter.java
+++ app/src/main/java/edu/neu/madcourse/studybuddy/GroupCardViewAdapter.java
@@ -1,4 +1,46 @@
 package edu.neu.madcourse.studybuddy;
 
-public class GroupCardViewAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+
+/**
+ * This class represents the adapter for the recycler view shic will be called by the main page
+ */
+public class GroupCardViewAdapter extends RecyclerView.Adapter<GroupCardViewHolder> {
+
+    private List<GroupCard> groupCards;
+    private AdapterView.OnItemClickListener listener;
+
+    public GroupCardViewAdapter(List<GroupCard> groupCards) {
+        this.groupCards = groupCards;
+    }
+
+    @NonNull
+    @Override
+    public GroupCardViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.group_card, parent, false);
+        return new GroupCardViewHolder(view, listener);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull GroupCardViewHolder holder, int position) {
+        GroupCard currentItem = groupCards.get(position);
+
+        holder.title.setText(currentItem.title);
+        holder.dateTime.setText(currentItem.date);
+        holder.location.setText(currentItem.location);
+    }
+
+    @Override
+    public int getItemCount() {
+        return groupCards.size();
+    }
 }
--- app/src/main/java/edu/neu/madcourse/studybuddy/HomeScreen.java
+++ app/src/main/java/edu/neu/madcourse/studybuddy/HomeScreen.java
@@ -1,5 +1,27 @@
 package edu.neu.madcourse.studybuddy;
 
-public class HomeScreen {
+import android.app.AppComponentFactory;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.List;
+
+/**
+ * A class that represents the home screen for the application.
+ */
+public class HomeScreen extends AppCompatActivity {
+    private List<GroupCard> groupCards;
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter recyclerViewAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+
+    //Only appears when the user is logged in/ the page redirects to the register /login page
+    private FloatingActionButton floatingActionButton;
+
+    private static final String KEY_OF_INSTANCE = "KEY_OF_INSTANCE";
+    private static final String NUMBER_OF_ITEMS = "NUMBER_OF_ITEMS";
 }
