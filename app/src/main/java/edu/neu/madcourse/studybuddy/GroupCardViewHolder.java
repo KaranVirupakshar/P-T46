cec69996a5d04210d33945ad9dda3e878001972d
nikhilfeb95 <nikhil.mollay@gmail.com>
2022-04-16 21:40:55 -0400

    Pushing initial view holder

---
+++ app/src/main/java/edu/neu/madcourse/studybuddy/GroupCardViewHolder.java
@@ -0,0 +1,29 @@
+package edu.neu.madcourse.studybuddy;
+
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class GroupCardViewHolder extends RecyclerView.ViewHolder {
+    public TextView title;
+    public TextView dateTime;
+    public TextView location;
+
+    public GroupCardViewHolder(@NonNull View itemView, final AdapterView.OnItemClickListener onItemClickListener) {
+        super(itemView);
+        title = itemView.findViewById(R.id.group_title);
+        dateTime = itemView.findViewById(R.id.group_datetime);
+        location = itemView.findViewById(R.id.location);
+
+        //Go to the chat activity for the group from here once its done
+        itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+}
